41e32ad2c6b881576c50a26675656db2cb3af2b6
nagimbsher <49290690+nagimb@users.noreply.github.com>
2024-06-16 19:55:27 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LinksComponent from './LinksComponent'; // Your main component
-import NotFound from './NotFound'; // 404 component
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import LinksComponent from './LinksComponent'; 
+import NotFound from './NotFound'; 
 
 const App = () => {
   return (
-    <Router basename="/support">
+    <Router>
       <Routes>
         <Route path="/" element={<LinksComponent />} />
         <Route path="*" element={<NotFound />} />
