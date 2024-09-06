867a963d88fd430fbd14503241b03894c4295979
nagimbsher <49290690+nagimb@users.noreply.github.com>
2024-09-07 00:21:22 +0300

    Updated Privacy Policy component and UI adjustments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LinksComponent from './LinksComponent';
 import PrivacyPolicy from './PrivacyPolicy';
 import ContactMe from './ContactMe';
+import Support from './Support'; // Ensure this is imported
 
 const App = () => {
   return (
@@ -10,10 +11,32 @@ const App = () => {
         <Route path="/" element={<LinksComponent />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/contact-me" element={<ContactMe />} />
+        <Route path="/support" element={<Support />} /> {/* Added route */}
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
+
+
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import LinksComponent from './LinksComponent';
+// import PrivacyPolicy from './PrivacyPolicy';
+// import ContactMe from './ContactMe';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<LinksComponent />} />
+//         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+//         <Route path="/contact-me" element={<ContactMe />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
 
---
+++ src/Support.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Support = () => {
+  return (
+    <div>
+      <h1>Support Page</h1>
+      <p>This is the support page content.</p>
+    </div>
+  );
+};
+
+export default Support;
